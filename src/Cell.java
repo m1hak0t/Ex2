e4e05162ec465d1048eb1d90295a1b026b44548f
m1hak0t <michael.bricker@msmail.ariel.ac.il>
2024-12-30 08:20:55 +0200

    basic math in progress

--- src/Cell.java
+++ src/Cell.java
@@ -1,9 +1,11 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
+import java.util.ArrayList;
+
 public class Cell {
     public static void main(String args []) {
-
+        BasicMath("1*1/8");
     }
     public static boolean isNumber(String text) {
         boolean ans = true;
@@ -68,7 +70,32 @@ public class Cell {
         } else throw new Exception("Invalid formula format");
         return ans;
     }
+    //Basic math function 3*2/28+6 = result
     private static void BasicMath(String text) {
         //Scan for the math operators
+        ArrayList<String> symbols = new ArrayList();
+        //Create a list of clean symbols
+        ArrayList<String> cleansymbols = new ArrayList();
+        //Create a list for results
+        ArrayList<Integer> results = new ArrayList();
+        //Add all the chars in a string to a list
+        for (int i = 0; i < text.length(); i += 1) {
+            symbols.add(Character.toString(text.charAt(i)));
+        }
+        //Consolidate the digits in the list 2,5 -> 25
+        String digits = "0123456789.";
+        StringBuilder str = new StringBuilder();
+        int index = 0;
+        for (int i = 0; i < symbols.size(); i += 1) {
+            index = 0;
+            if (digits.contains(symbols.get(i))) {
+
+            }
+
+        }
+
+        //Go through this list, in case / or * is found - > calculate what's on the left and on the right and add to a list, in case +/-
+        //is found, just add them to a list.
+
     }
 }
