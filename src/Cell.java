eba4290d80192a2a09214be5abfeff274567bfe5
m1hak0t <michael.bricker@msmail.ariel.ac.il>
2025-01-03 12:12:36 +0200

    Calculate finished, tests in progress

--- src/Cell.java
+++ src/Cell.java
@@ -1,11 +1,12 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Cell {
     public static void main(String args []) {
-        BasicMath("1*1/8");
+        System.out.println(DividerbyAction("1*1/875*2+8-9*12"));
     }
     public static boolean isNumber(String text) {
         boolean ans = true;
@@ -70,32 +71,196 @@ public class Cell {
         } else throw new Exception("Invalid formula format");
         return ans;
     }
-    //Basic math function 3*2/28+6 = result
-    private static void BasicMath(String text) {
+
+    //The function does this: "1*1/((875*2)+8-9*12)" -> 334 recursively untily gets rid of all the parenthasies
+    public static double Calculate(String text) {
+        double ans = 0;
+        ArrayList divided = DividerbyAction(text);
+        ArrayList<Integer> indexbox = new ArrayList<>();
+        //In case there are parenthasies - find the last parenthasies and calculate the expression inside them
+        for (int i = 0; i<text.length(); i+=1) {
+            //In case while looping through the expression ( is found
+            while (Character.toString(text.charAt(i)).equals("(")) {
+                //Add the found ( index to the indexbox
+                indexbox.add(i);
+                //This loop will run unil it finds the closing ) or finds one more opening (
+                i+=1;
+                //Move one step further
+                //Check the option with nested expressions
+                ///TOMORROW DO THIS
+                //Check the option without nested expressions
+                if (Character.toString(text.charAt(i)).equals(")")) {
+                    int left_bound = indexbox.get(1);
+                    int right_bound = i;
+                    //At this stage this is found (1*2) , the indexes are pointing to 1*2
+                    //Calculate this expression and turn it to double
+                    double result = Double.parseDouble(String.valueOf(Calculate(text)));
+                    //Remove the initial expression from the string
+                    StringBuffer textbuffer = new StringBuffer(text);
+                    textbuffer.delete(left_bound-1,right_bound+1);
+                    //Add the expression that we've calculated to a string
+                    textbuffer.insert(i,Double.toString(result));
+                    text = textbuffer.toString();
+                    //Clean the indexbox
+                    indexbox.clear();
+                    //Pass further into the recursion loop
+                    Calculate(text);
+                }
+        }
+            //Go through the text and check whether the parenthasies exist, if one is fo und -- >> check whether there is a closing one on the way, if yes -calculate the expression, if another opening one if found on the way - repeat the process4
+            //After the expression was replaced by a value - run the function calculate one more time on a result (recursion)
+        }
+        //Return the result of a looper (array) turned into string and parse Double'ed after.
+        ans = Double.parseDouble(looper(divided).get(0).toString());
+
+        return ans;
+    }
+    //The Function that does: "1*1/875*2+8-9*12" -> [1, *, 1, /, 875, *, 2, +, 8, -, 9, *, 12]
+    private static ArrayList<String> DividerbyAction(String text) {
         //Scan for the math operators
         ArrayList<String> symbols = new ArrayList();
-        //Create a list of clean symbols
-        ArrayList<String> cleansymbols = new ArrayList();
-        //Create a list for results
-        ArrayList<Integer> results = new ArrayList();
         //Add all the chars in a string to a list
         for (int i = 0; i < text.length(); i += 1) {
             symbols.add(Character.toString(text.charAt(i)));
         }
-        //Consolidate the digits in the list 2,5 -> 25
+        //Add one ~ at a last index for further logic
+        symbols.add("~");
+        System.out.println("Before consolidation"+symbols);
+        //Consolidate the digits in the list [2,5] -> [25]
         String digits = "0123456789.";
         StringBuilder str = new StringBuilder();
-        int index = 0;
+        //Create an arraylist of indexes
+        ArrayList<Integer> indexes = new ArrayList<>();
         for (int i = 0; i < symbols.size(); i += 1) {
-            index = 0;
             if (digits.contains(symbols.get(i))) {
-
+                //Add the digit to a stringbuilder
+                str.append(symbols.get(i));
+                System.out.println(str);
+                //Add it to the indexes list
+                indexes.add(i);
+                //Replace the digit with null
+                if (indexes.size() > 1) {
+                    symbols.set(i, null);
+                }
+            } else {
+                System.out.println(indexes);
+                //In case we are not a digit index in symbols - we should check whether the digit containing operation has happened in the past - Indexes list shouldn't be empty
+                if (!indexes.isEmpty()) {
+                    //Append the collected digits to symbols
+                    String collected = str.toString();
+                    //Place it to the index where initial digit was found
+                    int initial_index = indexes.get(0);
+                    symbols.set(initial_index,collected);
+                    //Clean the traces
+                    indexes.clear();
+                    str = new StringBuilder();
+                }
             }
-
         }
+        //System.out.println("After consolidation"+symbols);
+        //Remove nulls and tildas
+        for (int i=0;i<symbols.size();i+=1) {
+            symbols.remove(null);
+        }
+        symbols.remove("~");
+        //System.out.println("Return" + symbols);
+        return symbols;
 
-        //Go through this list, in case / or * is found - > calculate what's on the left and on the right and add to a list, in case +/-
-        //is found, just add them to a list.
+
 
     }
+    //The function does this "[2, *, 8, /, 875, *, 2]" -> "[367]" Without : ")","(".
+    public static ArrayList<String> looper(ArrayList<String> text) {
+        System.out.println(text);
+        while (text.contains("/")) {
+            //In case text contains / we want to loop through it, calculate the first couple of numbers and calculating, then one more time in case text has /
+            for (int i = 0; i < text.size(); i += 1) {
+                if (text.get(i).toString().equals("/")) {
+                    String newvalue = "";
+                    double number_before = Double.parseDouble(text.get(i - 1));
+                    double number_after = Double.parseDouble(text.get(i + 1));
+                    newvalue = Double.toString(number_before / number_after);
+                    int first_index = i - 1;
+                    text.set(first_index, newvalue);
+                    text.set(i, null);
+                    text.set(i + 1, null);
+                    break;
+                }
+            }
+            //Delete the nulls from the list in case break worked
+            for (int i=0;i<text.size();i+=1) {
+                text.remove(null);
+            }
+        }
+        while (text.contains("*")) {
+            //In case text contains / we want to loop through it, calculate the first couple of numbers and calculating, then one more time in case text has /
+            for (int i = 0; i < text.size(); i += 1) {
+                if (text.get(i).toString().equals("*")) {
+                    String newvalue = "";
+                    double number_before = Double.parseDouble(text.get(i - 1));
+                    double number_after = Double.parseDouble(text.get(i + 1));
+                    newvalue = Double.toString(number_before * number_after);
+                    int first_index = i - 1;
+                    text.set(first_index, newvalue);
+                    text.set(i, null);
+                    text.set(i + 1, null);
+                    break;
+                }
+            }
+            //Delete the nulls from the list in case break worked
+            for (int i=0;i<text.size();i+=1) {
+                text.remove(null);
+            }
+        }
+        while (text.contains("-")) {
+            //In case text contains / we want to loop through it, calculate the first couple of numbers and calculating, then one more time in case text has /
+            for (int i = 0; i < text.size(); i += 1) {
+                if (text.get(i).toString().equals("-")) {
+                    String newvalue = "";
+                    if (!text.isEmpty() && text.get(0).equals("-")) {
+                        double number_itself = Double.parseDouble(text.get(i+1));
+                        number_itself = 0 - number_itself;
+                        text.set((i), String.valueOf(number_itself));
+                        text.set(i+1,null);
+                        break;
+                    }
+                    double number_before = Double.parseDouble(text.get(i - 1));
+                    double number_after = Double.parseDouble(text.get(i + 1));
+                    newvalue = Double.toString(number_before - number_after);
+                    int first_index = i - 1;
+                    text.set(first_index, newvalue);
+                    text.set(i, null);
+                    text.set(i + 1, null);
+                    break;
+                }
+            }
+            //Delete the nulls from the list in case break worked
+            for (int i=0;i<text.size();i+=1) {
+                text.remove(null);
+            }
+        }
+        while (text.contains("+")) {
+            //In case text contains / we want to loop through it, calculate the first couple of numbers and calculating, then one more time in case text has /
+            for (int i = 0; i < text.size(); i += 1) {
+                if (text.get(i).toString().equals("+")) {
+                    String newvalue = "";
+                    double number_before = Double.parseDouble(text.get(i - 1));
+                    double number_after = Double.parseDouble(text.get(i + 1));
+                    newvalue = Double.toString(number_before + number_after);
+                    int first_index = i - 1;
+                    text.set(first_index, newvalue);
+                    text.set(i, null);
+                    text.set(i + 1, null);
+                    break;
+                }
+            }
+            //Delete the nulls from the list in case break worked
+            for (int i=0;i<text.size();i+=1) {
+                text.remove(null);
+            }
+        }
+        System.out.println(text);
+        return text;
+    }
+
 }
--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,76 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
 
-public class Test {
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LooperTest {
+    @Test
+    public void testBasicAddition() {
+        String input = "2+3";
+        assertEquals(5, Cell.Calculate(input));
+    }
+
+    @Test
+    public void testBasicSubtraction() {
+        String input = "5-3";
+        assertEquals(2, Cell.Calculate(input));
+    }
+
+    @Test
+    public void testBasicMultiplication() {
+        String input = "4*2";
+       assertEquals(8, Cell.Calculate(input));
+    }
+
+    @Test
+    public void testBasicDivision() {
+        String input = "8/2";
+        assertEquals(4, Cell.Calculate(input));
+    }
+
+    @Test
+    public void testComplexExpression() {
+        String input = "2+3*4-6/2";
+        assertEquals(11, Cell.Calculate(input));
+    }
+
+    @Test
+    public void testWithRationalNumbers() {
+        String input = "2.5*4";
+        assertEquals(10, Cell.Calculate(input));
+    }
+
+    @Test
+    public void testDivisionByRationalNumber() {
+        String input = "10/2.5";
+        assertEquals(4, Cell.Calculate(input));
+    }
+
+    @Test
+    public void testNegativeNumbers() {
+        String input = "-3+7";
+        assertEquals(4, Cell.Calculate(input));
+    }
+
+
+
+    @Test
+    public void testEdgeCaseSingleNumber() {
+        String input = "42";
+        assertEquals(42, Cell.Calculate(input));
+    }
+
+    @Test
+    public void testDecimalPrecision() {
+        String input = "10/3";
+        assertEquals(3.3333, Cell.Calculate(input), 0.0001);
+    }
+
+    @Test
+    public void testOperatorPrecedence() {
+        String input = "2+3*4";
+        assertEquals(14, Cell.Calculate(input));
+    }
 }
+
